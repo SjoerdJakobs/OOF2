747d2c9637154f42f753160c97b716fa2fa7c408
Sjoerd Jakobs <sjakobs@chello.nl>
2022-03-21 02:36:09 +0100

    -fixed building for 32 bit by adding 32bit glfw so it can build for 64 and 32 bit -added folders for 64 and 32 bit libraries, everything outside those seem to work for both and dont need 2 seperate files

--- src/framework/rendering/Rectangle.h
+++ src/framework/rendering/Rectangle.h
@@ -1,5 +1,29 @@
 #pragma once
+
+#include"Texture.h"
+#include"shaderClass.h"
+#include"VAO.h"
+#include"VBO.h"
+#include"EBO.h"
+//
+//// Vertices coordinates
+//GLfloat vertices[] =
+//{ //     COORDINATES     /        COLORS      /   TexCoord  //
+//	-0.5f, -0.5f, 0.0f,     1.0f, 0.0f, 0.0f,	0.0f, 0.0f, // Lower left corner
+//	-0.5f,  0.5f, 0.0f,     0.0f, 1.0f, 0.0f,	0.0f, 1.0f, // Upper left corner
+//	 0.5f,  0.5f, 0.0f,     0.0f, 0.0f, 1.0f,	1.0f, 1.0f, // Upper right corner
+//	 0.5f, -0.5f, 0.0f,     1.0f, 1.0f, 1.0f,	1.0f, 0.0f  // Lower right corner
+//};
+//
+//// Indices for vertices order
+//GLuint indices[] =
+//{
+//	0, 2, 1, // Upper triangle
+//	0, 3, 2 // Lower triangle
+//};
+
 class Rectangle
 {
+
 };
 
